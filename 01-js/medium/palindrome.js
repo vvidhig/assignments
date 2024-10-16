63930185049f55f29b807726a2dbc6bb61a9d621
Vidhi <vidhidoesnotreply@gmail.com>
2024-10-16 12:58:03 +0530

    trying

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,8 +4,30 @@
 */
 
 function isPalindrome(str) {
+  if(str.length === 0 || str.length === 1) 
+    return true;
   str = str.toLowerCase();
-  
+  str_rev = "";
+  str1 = "";
+  for(let i = 0; i < str.length; i++)
+  {
+    if(str[i]!=" " || str[i]!="!"  || str[i]!="@"  || str[i]!="."  || str[i]!=",")
+    {
+      str1 = str1 + str[i];
+    }
+  }
+  for(let i = str1.length - 1; i >= 0; i--)
+  {
+    str_rev = str_rev + str1[i];
+  }
+  console.log(str_rev);
+  console.log(str1);
+  if(str_rev === str1)
+    return true;
+  else
+    return false;
 }
+
+console.log(isPalindrome('A man, a plan, a canal. Panama'));
 
 module.exports = isPalindrome;
