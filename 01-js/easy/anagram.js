3d61559eafbe79da209328cd02b3669b52924b68
Vidhi <vidhidoesnotreply@gmail.com>
2024-10-16 10:52:12 +0530

    passed easy anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,17 +5,24 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
   if(str1.length != str2.length)
     return false;
   let c = 0;
   for(let i = 0; i < str1.length; i++) {
+    let k = 0;
     for(let j = 0; j < str2.length; j++) {
-      if(str1[i] === str2[j])
+      if(str1[i] === str2[j] && k===0)
+      {
         c+=1;
+        k = 1;
+      }
     }
   }
   if(c===str1.length) 
     return true;
+  else
+    return false;
 }
-
 module.exports = isAnagram;
